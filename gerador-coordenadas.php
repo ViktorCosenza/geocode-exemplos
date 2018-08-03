0b94b511eb5bb0500f1b64b2df621d6521b7e5de
Victor Cosenza <vcvdsilva@inf.ufpel.edu.br>
2018-08-03 15:08:35 -0300

    Tratamento de erros

--- gerador-coordenadas.php
+++ gerador-coordenadas.php
@@ -1,7 +1,5 @@
 <?PHP
-    //Para testar, altere o endereço abaixo 
-    echo getCoordinates('Pelotas, RS');
-    
+    echo getCoordinates('Gen osorio n 527, Pelotas, RS');
     function getCoordinates($address){
  
         $address = str_replace(" ", "+", $address); // replace all the white space with "+" sign to match with google   search pattern
@@ -9,12 +7,21 @@
         $url = "http://maps.google.com/maps/api/geocode/json?sensor=false&address=$address";
  
         $response = file_get_contents($url);
-        
-
         $json = json_decode($response,TRUE); //generate array object from the response from the web
-        if(empty($json['results'])){
+
+        if($json['status'] === 'ZERO_RESULTS')
             return 'Cannot find address';
+        if($json['status'] === 'OVER_QUERY_LIMIT'){
+            sleep(1);
+            for($i = 0; ($json['status'] !== 'OK') && $i < 3; $i++){
+                $response = file_get_contents($url);
+                $json = json_decode($response,TRUE); //generate array object from the response from the web
+            }
         }
+        
+        if($json['status'] !== 'OK')
+            return 'Erro ' . $json['status'];
+        
         return ($json['results'][0]['geometry']['location']['lat'].",".$json['results'][0]['geometry']['location']['lng']);
     }
 ?>
